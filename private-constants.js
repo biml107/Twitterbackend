87f3f846ef53cf40c418fdf6f0804de798849a3f
Bimal kumar Mahato <54972204+biml107@users.noreply.github.com>
2024-01-08 23:12:01 +0530

    Add files via upload

---
+++ private-constants.js
@@ -0,0 +1,10 @@
+const dbURIbiml20000=`mongodb+srv://bimal:bimalkr@cluster0.tlc4fmt.mongodb.net/twitter?retryWrites=true&w=majority`
+const SESSIONKEY=`Helloworld1`;
+
+const bmlkumar2000Project0URI=`mongodb+srv://bmlkumar:bimalkumar@cluster0.gpa2nsy.mongodb.net/twitter?retryWrites=true&w=majority`
+ 
+const bmlkumar2000username=`bmlkumar2000`;
+const password=`brother`;
+const mongodbcompass='mongodb://localhost:27017';
+//mongodb://localhost:27017
+module.exports= { dbURIbiml20000, SESSIONKEY, mongodbcompass,bmlkumar2000Project0URI };
